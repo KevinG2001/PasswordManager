958e1613fb2e8f18f704b869b42ded596ba4ba21
Kevin <kevinglennon01@gmail.com>
2023-10-13 21:46:48 +0100

    Account methods moved to its own file

---
+++ account.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Establish myDBection
+myDB = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    database=DB_NAME,
+    password=DB_PASSWORD
+)
+
+
+def createAccount():
+    cursor = myDB.cursor()  # Create a cursor
+    username = input("Please enter your username")
+    password = input("Please enter your password")
+    insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+    values = (username, password)
+    cursor.execute(insert_query, values)
+    myDB.commit()  # Commit the changes to the database
+    # Close cursor and myDBection when done
+    cursor.close()
+    myDB.close()
+
+
+def login():
+    cursor = myDB.cursor()
+    username = input("Enter username")
+    # password = input("Enter password")
+    # Make sure to use placeholders in your query to prevent SQL injection
+    searchUsername = "SELECT * from users WHERE username = %s"
+
+    # Execute the query with the username as a parameter
+    cursor.execute(searchUsername, (username,))
+
+    # Fetch the result (assuming you want to fetch one row)
+    result = cursor.fetchone()
+    print(result)
--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-import mysql.connector
-from dotenv import load_dotenv
-import os
+import account
 
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-# Establish connection
-conn = mysql.connector.connect(
-    host=DB_HOST,
-    user=DB_USER,
-    database=DB_NAME,
-    password=DB_PASSWORD
-)
-
-# Create a cursor
-cursor = conn.cursor()
-
-
-username = input("Please enter your username")
-password = input("Please enter your password")
-
-# Execute the INSERT statement
-insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-values = (username, password)
-cursor.execute(insert_query, values)
-
-# Commit the changes
-conn.commit()
-
-# Close cursor and connection when done
-cursor.close()
-conn.close()
+# Start of app
+userinput = input("1)Login\n2)Create Account")
+userinput = int(userinput)  # Convert the input to an integer
+if userinput == 1:
+    account.login()
+else:
+    account.createAccount()
